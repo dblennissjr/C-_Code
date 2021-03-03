5226f1d9bacb45aecc0dd2ba31a300cf054a93ae
dblennissjr <60030@outlook.com>
2021-03-03 02:19:29 +0000

    Modified string output for the empty array

--- containers.cpp
+++ containers.cpp
@@ -10,7 +10,7 @@ int main()
   Example[3] = 4;
   Example[4] = 5;
 
-  std::cout << "Is the array empty?  " << Example.empty() << std::endl;
+  std::cout << "Is the array empty?  0 = False, 1 = True -> " << Example.empty() << std::endl;
   std::cout << "Element at the front of the array: " << Example.front() << std::endl;
   std::cout << "Element at position 3 is: " << Example.at(3) << std::endl;
 
